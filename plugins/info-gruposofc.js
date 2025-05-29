5c005126adacc552f386050eee6c4672179d1ff2
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-05-28 22:57:39 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
 
-let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad.....*
+let grupos = `*Hola!, te invito a ser parte de nuestros gran ejército*
 
 - ${namegrupo}
 > *❀* ${gp1}
