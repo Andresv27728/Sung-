a9c2dfb867abdb48ca8c1cf30a03d10946712977
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-06-01 15:57:04 -0500

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -27,18 +27,18 @@ return res.data
 console.log(`Error : ${e}`)
 }}
   
-global.creador = 'Wa.me/393715279301'
+global.creador = 'Wa.me/573133374132'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
-global.asistencia = 'Wa.me/393715279301'
-global.namechannel = '=͟͟͞❀ sᥙmі - sᥲkᥙrᥲsᥲᥕᥲ  ⏤͟͟͞͞★'
-global.namechannel2 = '=͟͟͞❀ sᥙmі - sᥲkᥙrᥲsᥲᥕᥲ ⏤͟͟͞͞★'
-global.namegrupo = 'ᰔᩚ ᥡᥙkі sᥙ᥆ᥙ • ᥆𝖿іᥴіᥲᥣ ❀'
-global.namecomu = 'ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥴ᥆mᥙᥒі𝗍ᥡ ❀'
+global.asistencia = 'Wa.me/573133374132'
+global.namechannel = '=͟͟͞❀ sung Jim wo ⚔️⏤͟͟͞͞★'
+global.namechannel2 = '=͟͟͞❀ Tiburón 🦈 BOT★'
+global.namegrupo = 'ᰔᩚ Tiburón • sung Jim wo ⚔️ '
+global.namecomu = 'ᰔᩚ say team  • ᥴ᥆mᥙᥒі𝗍ᥡ ❗'
 global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
 
 global.canalIdM = ["120363402846939411@newsletter", "120363402846939411@newsletter"]
-global.canalNombreM = ["⏤͟͟͞͞Vivos Vivientes 🌻❀", "🌳 𝖵𝗂𝗏𝗈𝗌 𝖵𝗂𝗏𝗂𝖾𝗇𝗍𝖾𝗌 🍄"]
+global.canalNombreM = ["⏤͟͟͞͞sung Jim wo ⚔️ ", "💖💝 Y⃟o⃟ S⃟o⃟y⃟ Y⃟o⃟ 💝 💖"]
 global.channelRD = await getRandomChannel()
 
 global.d = new Date(new Date + 3600000)
@@ -61,16 +61,16 @@ global.emoji4 = '୨୧'
 global.emoji5 = '♡'
 global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
 
-global.wait = '❍ Espera un momento, soy lenta...';
-global.waitt = '❍ Espera un momento, soy lenta...';
-global.waittt = '❍ Espera un momento, soy lenta...';
-global.waitttt = '❍ Espera un momento, soy lenta...';
+global.wait = '❍ Espera un momento, soy lento...';
+global.waitt = '❍ Espera un momento, soy lento...';
+global.waittt = '❍ Espera un momento, soy lento...';
+global.waitttt = '❍ Espera un momento, soy lento...';
 
-var canal = 'https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w'  
-var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
-var git = 'https://github.com/The-King-Destroy'
-var github = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot' 
-let correo = 'thekingdestroy507@gmail.com'
+var canal = 'https://whatsapp.com/channel/0029Vb5atcVL7UVQwAB9tU2k'  
+var comunidad = 'https://whatsapp.com/channel/0029Vb5atcVL7UVQwAB9tU2k'
+var git = 'https://github.com/Andresv27728'
+var github = 'https://github.com/Andresv27728' 
+let correo = 'andrescamilovallejogomez701@gmail.com'
 global.redes = [canal, comunidad, git, github, correo].getRandom()
 
 let category = "imagen"
@@ -91,9 +91,9 @@ var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
 global.packsticker = `┊ ૮₍｡•́︿•̀｡₎ა *Mai Stickers*  
-╰┈➤ ୨୧ https://whatsapp.com/channel/0029Vb5UfTC4CrfeKSamhp1f ✿
+╰┈➤ ୨୧ https://whatsapp.com/channel/0029Vb5atcVL7UVQwAB9tU2k ✿
 ┊ ℹ️ *Info:*  
-╰┈➤ 𖥻 Mai By Wirk 🪴`;
+╰┈➤ 𖥻 sung Jim wo By 💖💝 Y⃟o⃟ S⃟o⃟y⃟ Y⃟o⃟ 💝 💖`;
 
 global.packsticker2 = `\n┊🌸 Bot: ${botname}
 ┊🌷 Usuario: ${nombre}
